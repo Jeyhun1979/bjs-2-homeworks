0866e1a532aaa4522911adbcaa6ba44f547ca70a
Jeyhun Mamedov <89056658707@mail.ru>
2025-07-09 17:32:53 +0300

    Решение задачи №3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const result = users
+    .filter(user => user.gender === gender)
+    .reduce((acc, user, index, arr) => {
+      acc += user.age;
+      if (index === arr.length - 1) {
+      return acc / arr.length;
+     }
+      return acc;
+     }, 0)
+    return result;
 }
